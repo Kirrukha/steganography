bb814695500b83c7004a2df2c7b7620bc33905c6
Kirrukha <34424437+Kirrukha@users.noreply.github.com>
2019-06-10 16:02:46 +0300

    Add files via upload

---
+++ Kursach/Form1.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace Kursach
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnCleanE = new System.Windows.Forms.Button();
+            this.btnHide = new System.Windows.Forms.Button();
+            this.txtPassE = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSendE = new System.Windows.Forms.TextBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.btnCleanD = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSendD = new System.Windows.Forms.TextBox();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.txtPassD = new System.Windows.Forms.TextBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.txtFolder = new System.Windows.Forms.TextBox();
+            this.btnFolder = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.groupBox3);
+            this.groupBox1.Controls.Add(this.groupBox4);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.txtFolder);
+            this.groupBox1.Controls.Add(this.btnFolder);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Constantia", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox1.Location = new System.Drawing.Point(15, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(356, 359);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnCleanE);
+            this.groupBox3.Controls.Add(this.btnHide);
+            this.groupBox3.Controls.Add(this.txtPassE);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Controls.Add(this.txtSendE);
+            this.groupBox3.Location = new System.Drawing.Point(7, 154);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(330, 186);
+            this.groupBox3.TabIndex = 7;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Введите сообщение:";
+            // 
+            // btnCleanE
+            // 
+            this.btnCleanE.Location = new System.Drawing.Point(191, 125);
+            this.btnCleanE.Name = "btnCleanE";
+            this.btnCleanE.Size = new System.Drawing.Size(114, 53);
+            this.btnCleanE.TabIndex = 9;
+            this.btnCleanE.Text = "Очистить поля";
+            this.btnCleanE.UseVisualStyleBackColor = true;
+            this.btnCleanE.Click += new System.EventHandler(this.btnCleanE_Click);
+            // 
+            // btnHide
+            // 
+            this.btnHide.Location = new System.Drawing.Point(8, 127);
+            this.btnHide.Name = "btnHide";
+            this.btnHide.Size = new System.Drawing.Size(114, 53);
+            this.btnHide.TabIndex = 8;
+            this.btnHide.Text = "Спрятать тайник!";
+            this.btnHide.UseVisualStyleBackColor = true;
+            this.btnHide.Click += new System.EventHandler(this.btnHide_Click);
+            // 
+            // txtPassE
+            // 
+            this.txtPassE.Location = new System.Drawing.Point(7, 88);
+            this.txtPassE.Name = "txtPassE";
+            this.txtPassE.Size = new System.Drawing.Size(311, 23);
+            this.txtPassE.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Пароль:";
+            // 
+            // txtSendE
+            // 
+            this.txtSendE.Location = new System.Drawing.Point(7, 23);
+            this.txtSendE.Name = "txtSendE";
+            this.txtSendE.Size = new System.Drawing.Size(311, 23);
+            this.txtSendE.TabIndex = 5;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.btnCleanD);
+            this.groupBox4.Controls.Add(this.label3);
+            this.groupBox4.Controls.Add(this.txtSendD);
+            this.groupBox4.Controls.Add(this.btnShow);
+            this.groupBox4.Controls.Add(this.txtPassD);
+            this.groupBox4.Location = new System.Drawing.Point(12, 154);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(330, 197);
+            this.groupBox4.TabIndex = 9;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Введите пароль:";
+            // 
+            // btnCleanD
+            // 
+            this.btnCleanD.Location = new System.Drawing.Point(203, 61);
+            this.btnCleanD.Name = "btnCleanD";
+            this.btnCleanD.Size = new System.Drawing.Size(114, 53);
+            this.btnCleanD.TabIndex = 10;
+            this.btnCleanD.Text = "Очистить поля";
+            this.btnCleanD.UseVisualStyleBackColor = true;
+            this.btnCleanD.Click += new System.EventHandler(this.btnCleanD_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(168, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Спрятанное сообщение:";
+            // 
+            // txtSendD
+            // 
+            this.txtSendD.Location = new System.Drawing.Point(6, 145);
+            this.txtSendD.Name = "txtSendD";
+            this.txtSendD.ReadOnly = true;
+            this.txtSendD.Size = new System.Drawing.Size(311, 23);
+            this.txtSendD.TabIndex = 7;
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(6, 61);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(114, 47);
+            this.btnShow.TabIndex = 8;
+            this.btnShow.Text = "Раскрыть дело!";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // txtPassD
+            // 
+            this.txtPassD.Location = new System.Drawing.Point(7, 23);
+            this.txtPassD.Name = "txtPassD";
+            this.txtPassD.Size = new System.Drawing.Size(311, 23);
+            this.txtPassD.TabIndex = 5;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(12, 115);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(90, 20);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Извлечь";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 89);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(105, 20);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Записать";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // txtFolder
+            // 
+            this.txtFolder.Location = new System.Drawing.Point(12, 45);
+            this.txtFolder.Name = "txtFolder";
+            this.txtFolder.ReadOnly = true;
+            this.txtFolder.Size = new System.Drawing.Size(224, 23);
+            this.txtFolder.TabIndex = 2;
+            // 
+            // btnFolder
+            // 
+            this.btnFolder.Location = new System.Drawing.Point(244, 43);
+            this.btnFolder.Name = "btnFolder";
+            this.btnFolder.Size = new System.Drawing.Size(87, 25);
+            this.btnFolder.TabIndex = 1;
+            this.btnFolder.Text = "Обзор";
+            this.btnFolder.UseVisualStyleBackColor = true;
+            this.btnFolder.Click += new System.EventHandler(this.btnFolder_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Выберите видеофайл:";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Constantia", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.richTextBox1.Location = new System.Drawing.Point(378, 6);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(454, 358);
+            this.richTextBox1.TabIndex = 10;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(843, 373);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Constantia", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Name = "Form1";
+            this.Text = "Стеганография";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnFolder;
+        private System.Windows.Forms.TextBox txtFolder;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnHide;
+        private System.Windows.Forms.TextBox txtPassE;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSendE;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSendD;
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.TextBox txtPassD;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnCleanD;
+        private System.Windows.Forms.Button btnCleanE;
+    }
+}
+
---
+++ Kursach/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Kursach
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            groupBox3.Visible = true;
+            groupBox4.Visible = false;
+            radioButton1.Checked = true;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                groupBox3.Visible = true;
+                groupBox4.Visible = false;
+                txtPassE.Text = string.Empty;
+                txtSendE.Text = string.Empty;
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked)
+            {
+                groupBox4.Visible = true;
+                groupBox3.Visible = false;
+                txtPassD.Text = string.Empty;
+                txtSendD.Text = string.Empty;
+            }
+        }
+
+        private void btnFolder_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.InitialDirectory = "D:\\";
+            openFileDialog1.Filter = "MP4 files (*.MP4)|*.MP4|All files (*.*)|*.*";
+            DialogResult result = openFileDialog1.ShowDialog();
+
+            //Если нажата клавиша ОК
+            if (result == DialogResult.OK)
+            {
+                string openFileName = openFileDialog1.FileName;
+                txtFolder.Text = openFileName;
+                refreshBox();
+                Invalidate();
+            }
+
+            // Если нажата клавиша Отмена
+            else if (result == DialogResult.Cancel)
+            {
+                return;
+            }
+        }
+
+        private void btnHide_Click(object sender, EventArgs e)
+        {
+            var tfile = TagLib.File.Create(txtFolder.Text);
+            if (txtSendE.Text != string.Empty && txtPassE.Text != string.Empty)
+            {
+                if (tfile.Tag.Album == null && tfile.Tag.Conductor == null)
+                {
+                    tfile.Tag.Album = txtSendE.Text;
+                    tfile.Tag.Conductor = txtPassE.Text;
+                    tfile.Save();
+                    txtSendE.Text = "Admin> Успех!";
+                    txtPassE.Text = "Admin> Операция выполнена";
+                    refreshBox();
+                }
+                else if (tfile.Tag.AlbumSort == null && tfile.Tag.AmazonId == null)
+                {
+                    tfile.Tag.AlbumSort = txtSendE.Text;
+                    tfile.Tag.AmazonId = txtPassE.Text;
+                    tfile.Save();
+                    txtSendE.Text = "Admin> Успех!";
+                    txtPassE.Text = "Admin> Операция выполнена";
+                    refreshBox();
+                }
+                else
+                {
+                    txtSendE.Text = "Admin> Ошибка!";
+                    txtPassE.Text = "Admin> Нет места для записи";
+                    refreshBox();
+                }
+            }
+            else
+            {
+                txtSendE.Text = "Admin> Ошибка!";
+                txtPassE.Text = "Admin > Незаполненные поля";
+            }
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            var tfile = TagLib.File.Create(txtFolder.Text);
+
+            if (txtPassD.Text == tfile.Tag.Conductor)
+            {
+                txtSendD.Text = tfile.Tag.Album;
+                tfile.Tag.Conductor = string.Empty;
+                tfile.Tag.Album = string.Empty;
+                tfile.Save();
+                refreshBox();
+            }
+            else if (txtPassD.Text == tfile.Tag.AmazonId)
+            {
+                txtSendD.Text = tfile.Tag.AlbumSort;
+                tfile.Tag.AlbumSort = string.Empty;
+                tfile.Tag.AmazonId = string.Empty;
+                tfile.Save();
+                refreshBox();
+            }
+            else
+                txtSendD.Text = "Admin> Неверный пароль!";
+        }
+        public void refreshBox()
+        {
+            var tfile = TagLib.File.Create(txtFolder.Text);
+
+            string send2 = tfile.Tag.AlbumSort;
+            string pass2 = tfile.Tag.AmazonId;
+
+            string send = tfile.Tag.Album;
+            string pass = tfile.Tag.Conductor;
+
+            string title = tfile.Tag.Title;
+            TimeSpan duration = tfile.Properties.Duration;
+            string description = tfile.Properties.Description;
+            int bitRate = tfile.Properties.AudioBitrate;
+            int vidHeight = tfile.Properties.VideoHeight;
+            int vidWidth = tfile.Properties.VideoWidth;
+
+            string checky = string.Empty;
+            if (send != null && pass != null)
+                checky = " ✓ ";
+            string checky2 = string.Empty;
+            if (send2 != null && pass2 != null)
+                checky2 = " ✓ ";
+
+            richTextBox1.Text = "Название: " + checky + title + " " + checky2 +
+                " \nПродолжительность: " + duration + " \nОписание: " + description +
+                "\nБитрейт: " + bitRate + " \nРазрешение: " + vidWidth + " х " + vidHeight;
+           // + "\n\nLetter: " + send + "\n\nPassword: " + pass + "\n\nLetter2: " + send2 + "\n\nPassword2: " + pass2;
+        }
+
+        private void btnCleanD_Click(object sender, EventArgs e)
+        {
+            txtPassD.Text = string.Empty;
+            txtSendD.Text = string.Empty;
+        }
+
+        private void btnCleanE_Click(object sender, EventArgs e)
+        {
+            txtPassE.Text = string.Empty;
+            txtSendE.Text = string.Empty;
+        }
+    }
+}
